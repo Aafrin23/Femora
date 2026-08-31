4f5c8479857211242d08428aa27ef0013ce1091d
Aafrin23 <aafrinbanu.j@gmail.com>
2026-08-31 19:31:53 +0530

    Connect frontend to deployed backend

--- frontend/src/api/User.js
+++ frontend/src/api/User.js
@@ -1,7 +1,14 @@
+// import axios from "axios";
+
+// const API = axios.create({
+//   baseURL: "http://localhost:5000/api",
+// });
+
+// export default API;
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://femora-hlt8.vercel.app/api",
 });
 
 export default API;
